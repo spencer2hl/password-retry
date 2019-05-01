9624ab314a8bcf701a7ad438584783c608350509
spencer2hl <frost0901@outlook.com>
2019-05-01 03:45:26 -0500

    change to while i > 0

--- day2exe.py
+++ day2exe.py
@@ -7,13 +7,16 @@
 
 password = 'a123456'
 i = 3 
-while True: 
+while i > 0: 
+	i = i - 1 
 	pwd = input('请输入密码：')
 	if pwd == password:
 		print('登入成功！')
 		break # 逃出回圈
 	else:
-		i = i - 1
-		print('密码错误！还有', i, '次机会')
-		if i == 0:
-			break 
+		print('密码错误！')
+		if i > 0:
+			print('还有', i, '次机会')
+		else:
+			print('没机会尝试了！ 要锁账号啦')
+
